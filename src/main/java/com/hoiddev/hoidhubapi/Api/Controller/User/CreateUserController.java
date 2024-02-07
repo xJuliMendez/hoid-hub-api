39b8fb91567b5b1740822dd87ff5ae41c6f84a01
Tu Nombre <you@example.com>
2024-02-07 21:32:24 +0100

    first ws config

--- src/main/java/com/hoiddev/hoidhubapi/Api/Controller/User/CreateUserController.java
+++ src/main/java/com/hoiddev/hoidhubapi/Api/Controller/User/CreateUserController.java
@@ -2,8 +2,6 @@ package com.hoiddev.hoidhubapi.Api.Controller.User;
 
 import com.hoiddev.hoidhubapi.Business.User.Dto.CreateUserInDto;
 import com.hoiddev.hoidhubapi.Business.User.Service.CreateUserService;
-import com.hoiddev.hoidhubapi.Business.User.Service.GetUserService;
-import com.hoiddev.hoidhubapi.Data.Database.Entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
